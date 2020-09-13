f41c8bc0afdccba7a0fcbb77512ebd248083f3e2
Alejandro Olivares <alejandroolivares@MacBook-Pro-de-Alejandro.local>
2020-09-13 17:35:15 -0500

    Building successfully, now the fun begins.

--- MVVM-C_with_Swift/Source/Dashboard/Widgets/Users/View/UsersViewController.swift
+++ MVVM-C_with_Swift/Source/Dashboard/Widgets/Users/View/UsersViewController.swift
@@ -15,19 +15,19 @@ final class UsersViewController: BaseViewController<UsersViewModelType> {
     @IBOutlet private weak var userFoundLabel: UILabel!
 
     private func bindToViewModel() {
-        viewModel.rx_usersCountInfo
-            .drive(usersCountInfoLabel.rx.text)
-            .disposed(by: disposeBag)
-
-        viewModel.bindUserSearchQuery(searchUserTextField.rx.text.orEmpty.asObservable())
-
-        viewModel.rx_userFound
-            .drive(userFoundLabel.rx.text)
-            .addDisposableTo(disposeBag)
-
-        viewModel.rx_userFoundDefault
-            .drive(userFoundLabel.rx.text)
-            .addDisposableTo(disposeBag)
+//        viewModel.rx_usersCountInfo
+//            .drive(usersCountInfoLabel.rx.text)
+//            .disposed(by: disposeBag)
+//
+//        viewModel.bindUserSearchQuery(searchUserTextField.rx.text.orEmpty.asObservable())
+//
+//        viewModel.rx_userFound
+//            .drive(userFoundLabel.rx.text)
+//            .addDisposableTo(disposeBag)
+//
+//        viewModel.rx_userFoundDefault
+//            .drive(userFoundLabel.rx.text)
+//            .addDisposableTo(disposeBag)
     }
 
     override func viewDidLoad() {
@@ -42,25 +42,25 @@ final class UsersViewController: BaseViewController<UsersViewModelType> {
     }
 
     private func addTapGesture() {
-        let tapBackground = UITapGestureRecognizer()
-        tapBackground.rx.event
-            .subscribe(onNext: { [weak self] _ in
-                self?.view.endEditing(true)
-            })
-            .disposed(by: disposeBag)
-        view.addGestureRecognizer(tapBackground)
+//        let tapBackground = UITapGestureRecognizer()
+//        tapBackground.rx.event
+//            .subscribe(onNext: { [weak self] _ in
+//                self?.view.endEditing(true)
+//            })
+//            .disposed(by: disposeBag)
+//        view.addGestureRecognizer(tapBackground)
     }
 }
 
 // MARK: - Custom bindings
 extension UsersViewController {
     fileprivate func bindActivityIndicatorVisibility() {
-        viewModel.rx_shouldShowActivityIndicator
-            .distinctUntilChanged()
-            .drive(onNext: { [weak self] shouldShow in
-                self?.updateActivityIndicatorVisibility(shouldShow)
-            })
-            .disposed(by: disposeBag)
+//        viewModel.rx_shouldShowActivityIndicator
+//            .distinctUntilChanged()
+//            .drive(onNext: { [weak self] shouldShow in
+//                self?.updateActivityIndicatorVisibility(shouldShow)
+//            })
+//            .disposed(by: disposeBag)
     }
 
     fileprivate func updateActivityIndicatorVisibility(_ shouldShow: Bool) {
@@ -72,12 +72,12 @@ extension UsersViewController {
     }
 
     fileprivate func bindUsersSearchView() {
-        viewModel.rx_shouldShowUsersSearchView
-            .distinctUntilChanged()
-            .drive(onNext: { [weak self] shouldShow in
-                self?.updateUsersSearchViewVisibility(shouldShow)
-            })
-            .disposed(by: disposeBag)
+//        viewModel.rx_shouldShowUsersSearchView
+//            .distinctUntilChanged()
+//            .drive(onNext: { [weak self] shouldShow in
+//                self?.updateUsersSearchViewVisibility(shouldShow)
+//            })
+//            .disposed(by: disposeBag)
     }
 
     fileprivate func updateUsersSearchViewVisibility(_ shouldShow: Bool) {
